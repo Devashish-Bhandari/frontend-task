b35221f58c32332f920ef13c15dd3cc4dcf88eda
Devashish Bhandari <devashishbhandari100@gmail.com>
2023-04-06 16:47:29 +0530

    Final changes made

--- main.js
+++ main.js
@@ -1,54 +1,67 @@
 const cells= document.querySelectorAll('.cell');
 const boxes= document.querySelectorAll('.box');
-let sourceBox= null;
-let destinationBox= null;
-let sourceCell= null;
-let destinationCell= null;
 
+let sourceBox= null; // The box which is being dragged
+let destinationBox= null; // The box at the cell where dragged box will be dropped
+let sourceCell= null; // The parent cell of the dragged box
+let destinationCell= null; // The cell on which source box is dropped
+
+// This array will store the ids of the swapped boxes
+// Through this, we will be able to track the swapping events,
+// and therefore be able to undo the actions
 let eventsArr= [];
 
+// DRAG AND DROP FUNCTIONALITY
+
+// Applying Event Listeners to All the Boxes in the Cells
 boxes.forEach( (box) => {
+
+    // When the box starts being dragged
     box.addEventListener('dragstart', ()=> {
         box.classList.add('is-dragging');
         sourceBox= box;
         sourceCell= box.parentElement;
     })
+    // When the box is released
     box.addEventListener('dragend', ()=> {
         box.classList.remove('is-dragging')
     })
 })
 
+// Adding Event Listeners to the Cells of the Table
 cells.forEach( (cell) => {
-    // when draggable element is over a box element
+
+    // When the draggable box is over the cell
     cell.addEventListener('dragover', (e) => {
         e.preventDefault();
         cell.classList.add('hovered');
     })
 
-    // when draggable element leaves the box element
+    // When the draggable box leaves that cell
     cell.addEventListener('dragleave', ()=> {
         cell.classList.remove('hovered');
     })
 
-    // when the draggable element drops on the box element
+    // When the draggable box is dropped on the cell
     cell.addEventListener('drop', ()=>{
-        destinationCell= cell;
-        destinationBox= cell.firstElementChild;
 
-        // console.log("destination box " , destinationBox.id);
-        // console.log("source box " , sourceBox.id);
+        destinationCell= cell; // destination cell where box is dropped
+        destinationBox= cell.firstElementChild; // box which is already present inside that cell 
 
+        // To omit the condition that the cell is dropped on it's own parent cell
         if(sourceBox !== destinationBox){
+            // Adding to the Events Array (for Undo to function)
             eventsArr.push( { 
                 sourceBoxId : sourceBox.id, 
                 destinationBoxId: destinationBox.id
             } );
         }
 
-
+        // Performing the drag and drop
         cell.appendChild(sourceBox);
         sourceCell.appendChild(destinationBox);
 
+        // Removing Hovered class after the draga and drop is performed
         cell.classList.remove('hovered');
     })
 })
@@ -58,30 +71,36 @@ cells.forEach( (cell) => {
 
 let undoBtn= document.querySelector('.undo');
 
+// Applying event listener to the Undo button
 undoBtn.addEventListener('click', ()=>{
+
+    // Performing the Undo function only if Drag and Drop has been performed previously
     if(eventsArr.length>0){
         
+        // Taking the last drag and drop from the array
         let i= eventsArr.length-1;
+        
+        // Destructuring SourceBox and DestinationBox ids.
         let { sourceBoxId , destinationBoxId} = eventsArr[i];
-        console.log(i);
-        console.log(sourceBoxId, destinationBoxId);
     
+        // Storing the elements with these Ids
         let boxA= document.querySelector(`#${sourceBoxId}`);
         let boxB= document.querySelector(`#${destinationBoxId}`);
     
+        // Storing the current Parent Cells of these boxes
         let cellA= boxA.parentElement;
         let cellB= boxB.parentElement;
     
+        // Undoing the Drag and Drop
         cellA.appendChild(boxB);
         cellB.appendChild(boxA);
     
+        // Removing this event from the array as this event is now undone
         eventsArr.pop();
     
-        console.log(boxA, boxB);
-
     }
     else{
-        alert("No Swapping performed so far");
+        alert("No Drag and Drop performed so far.");
     }
 
 })
