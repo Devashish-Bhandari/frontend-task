d999eb1fd349fda116cb771f2e6c12fbf4966d77
Devashish Bhandari <devashishbhandari100@gmail.com>
2023-04-06 15:17:40 +0530

    added the undo functionality

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let eventsArr= [];
 boxes.forEach( (box) => {
     box.addEventListener('dragstart', ()=> {
         box.classList.add('is-dragging');
-        draggedBox= box;
+        sourceBox= box;
         sourceCell= box.parentElement;
     })
     box.addEventListener('dragend', ()=> {
@@ -34,10 +34,55 @@ cells.forEach( (cell) => {
     cell.addEventListener('drop', ()=>{
         destinationCell= cell;
         destinationBox= cell.firstElementChild;
-        cell.appendChild(draggedBox);
-        console.log(sourceCell, destinationBox);
+
+        // console.log("destination box " , destinationBox.id);
+        // console.log("source box " , sourceBox.id);
+
+        if(sourceBox !== destinationBox){
+            eventsArr.push( { 
+                sourceBoxId : sourceBox.id, 
+                destinationBoxId: destinationBox.id
+            } );
+        }
+
+
+        cell.appendChild(sourceBox);
         sourceCell.appendChild(destinationBox);
+
         cell.classList.remove('hovered');
     })
+})
+
+
+// UNDO FUNCTIONALITY
+
+let undoBtn= document.querySelector('.undo');
+
+undoBtn.addEventListener('click', ()=>{
+    if(eventsArr.length>0){
+        
+        let i= eventsArr.length-1;
+        let { sourceBoxId , destinationBoxId} = eventsArr[i];
+        console.log(i);
+        console.log(sourceBoxId, destinationBoxId);
+    
+        let boxA= document.querySelector(`#${sourceBoxId}`);
+        let boxB= document.querySelector(`#${destinationBoxId}`);
+    
+        let cellA= boxA.parentElement;
+        let cellB= boxB.parentElement;
+    
+        cellA.appendChild(boxB);
+        cellB.appendChild(boxA);
+    
+        eventsArr.pop();
+    
+        console.log(boxA, boxB);
+
+    }
+    else{
+        alert("No Swapping performed so far");
+    }
+
 })
 
